a5514e8481929085e0e537bae7ad6c5e9ef198fa
Tyson Whitehead <twhitehead@gmail.com>
2018-04-10 16:49:59 -0400

    Rewrite as a unique data instance and not a (Bool,a) wrapper
    The (Bool,a) wrapper had to provide a default value for a.  This
    avoids this problem by only reading/writing a if Bool is True.

--- Data/Vector/Unboxed/Maybe.hs
+++ Data/Vector/Unboxed/Maybe.hs
@@ -11,7 +11,7 @@
 -- Maintainer  : twhitehead@gmail.com
 -- Portability : non-portable
 --
--- Adaptive unboxed vectors: add Maybe instance based on Complex and Bool
+-- Adaptive unboxed vectors: add Maybe instance based on the (a,b) 2-tuple instance.
 --
 
 module Data.Vector.Unboxed.Maybe
@@ -40,81 +40,134 @@ import           Control.Monad.ST ( runST, ST )
 -- The unboxed `Bool` vector is currently just a unboxed `Word8` vector internally.  This uses 8x more memory for
 -- the mask then technically required, so a future option would be to switch to using the `Data.Bit` from bitvec.
 
-newtype instance U.MVector s (Maybe a) = MV_Maybe (U.MVector s (Bool,a))
-newtype instance U.Vector    (Maybe a) = V_Maybe  (U.Vector    (Bool,a))
+data instance U.MVector s (Maybe a) = MV_Maybe {-# UNPACK #-} !Int !(U.MVector s Bool) !(U.MVector s a)
+data instance U.Vector    (Maybe a) = V_Maybe  {-# UNPACK #-} !Int !(U.Vector    Bool) !(U.Vector    a)
 
 instance (U.Unbox a) => U.Unbox (Maybe a)
 
 
--- | `initialized` provides a value for @a@ when tranlating @Nothing@ to @(False,a)@.
+-- Standard `M.MVector` and `G.Vector` boilerplate, mostly lifted from the @(a,b)@ 2-tuple instance.
 --
--- Use the default @Unbox a@ initialized value.  Digging through the code reveals this is just whatever you cast
--- from a memory that is all zeros (for most integer and float encodings this equals 0).
+-- The 2-tuple code is found under /internal\/unbox-tuple-instances/ in the vector source tree.  As little was
+-- changed as possible (@(a,b)@ to @(Bool,b)@ and the read/write functions) to make it easy to sync.
 
-initialized :: forall a. U.Unbox a => a
-initialized = runST $ do
-  vector <- M.new 1 :: forall s. ST s (U.MVector s a)
-  value  <- M.read vector 1
-  return value
+instance (U.Unbox b) => M.MVector U.MVector (Maybe b) where
+  {-# INLINE basicLength  #-}
+  basicLength (MV_Maybe n_ _ _) = n_
+  {-# INLINE basicUnsafeSlice  #-}
+  basicUnsafeSlice i_ m_ (MV_Maybe _ as bs)
+      = MV_Maybe m_ (M.basicUnsafeSlice i_ m_ as)
+                    (M.basicUnsafeSlice i_ m_ bs)
+  {-# INLINE basicOverlaps  #-}
+  basicOverlaps (MV_Maybe _ as1 bs1) (MV_Maybe _ as2 bs2)
+      = M.basicOverlaps as1 as2
+        || M.basicOverlaps bs1 bs2
+  {-# INLINE basicUnsafeNew  #-}
+  basicUnsafeNew n_
+      = do
+          as <- M.basicUnsafeNew n_
+          bs <- M.basicUnsafeNew n_
+          return $ MV_Maybe n_ as bs
+  {-# INLINE basicInitialize  #-}
+  basicInitialize (MV_Maybe _ as bs)
+      = do
+          M.basicInitialize as
+          M.basicInitialize bs
+  {-# INLINE basicUnsafeReplicate  #-}
+  basicUnsafeReplicate n_ (Just b)
+      = do
+          as <- M.basicUnsafeReplicate n_ True
+          bs <- M.basicUnsafeReplicate n_ b
+          return $ MV_Maybe n_ as bs
+  basicUnsafeReplicate n_ Nothing
+      = do
+          as <- M.basicUnsafeReplicate n_ False
+          bs <- M.basicUnsafeNew       n_
+          return $ MV_Maybe n_ as bs
+  {-# INLINE basicUnsafeRead  #-}
+  basicUnsafeRead (MV_Maybe _ as bs) i_
+      = do
+          a <- M.basicUnsafeRead as i_
+          if a
+            then do
+              b <- M.basicUnsafeRead bs i_
+              return (Just b)
+            else do
+              return Nothing
+  {-# INLINE basicUnsafeWrite  #-}
+  basicUnsafeWrite (MV_Maybe _ as bs) i_ (Just b)
+      = do
+          M.basicUnsafeWrite as i_ True
+          M.basicUnsafeWrite bs i_ b
+  basicUnsafeWrite (MV_Maybe _ as bs) i_ Nothing
+      = do
+          M.basicUnsafeWrite as i_ False
+  {-# INLINE basicClear  #-}
+  basicClear (MV_Maybe _ as bs)
+      = do
+          M.basicClear as
+          M.basicClear bs
+  {-# INLINE basicSet  #-}
+  basicSet (MV_Maybe _ as bs) (Just b)
+      = do
+          M.basicSet as True
+          M.basicSet bs b
+  basicSet (MV_Maybe _ as bs) Nothing
+      = do
+          M.basicSet as False
+  {-# INLINE basicUnsafeCopy  #-}
+  basicUnsafeCopy (MV_Maybe _ as1 bs1) (MV_Maybe _ as2 bs2)
+      = do
+          M.basicUnsafeCopy as1 as2
+          M.basicUnsafeCopy bs1 bs2
+  {-# INLINE basicUnsafeMove  #-}
+  basicUnsafeMove (MV_Maybe _ as1 bs1) (MV_Maybe _ as2 bs2)
+      = do
+          M.basicUnsafeMove as1 as2
+          M.basicUnsafeMove bs1 bs2
+  {-# INLINE basicUnsafeGrow  #-}
+  basicUnsafeGrow (MV_Maybe n_ as bs) m_
+      = do
+          as' <- M.basicUnsafeGrow as m_
+          bs' <- M.basicUnsafeGrow bs m_
+          return $ MV_Maybe (m_+n_) as' bs'
 
-
--- | `fromMaybe` translate from @Maybe a@ to @(Bool,a)@ using `initialzed` for masked @a@ values.
-
-fromMaybe :: U.Unbox a => Maybe a -> (Bool,a)
-{-# INLINE fromMaybe #-}
-fromMaybe (Just a) = (True ,a)
-fromMaybe Nothing  = (False,initialized)
-
-
--- | `toMaybe` translate from @(Bool,a)@ to @Maybe a@ by treating the @Bool@ as a mask.
-
-toMaybe :: (Bool,a) -> Maybe a
-toMaybe (True ,a) = Just a
-toMaybe (False,_) = Nothing
-{-# INLINE toMaybe #-}
-
-
--- Standard `M.MVector` and `G.Vector` boilerplate, mostly lifted from `Complex` and `Bool` instances.
-
-instance (U.Unbox a) => M.MVector U.MVector (Maybe a) where
-  {-# INLINE basicLength #-}
-  {-# INLINE basicUnsafeSlice #-}
-  {-# INLINE basicOverlaps #-}
-  {-# INLINE basicUnsafeNew #-}
-  {-# INLINE basicInitialize #-}
-  {-# INLINE basicUnsafeReplicate #-}
-  {-# INLINE basicUnsafeRead #-}
-  {-# INLINE basicUnsafeWrite #-}
-  {-# INLINE basicClear #-}
-  {-# INLINE basicSet #-}
-  {-# INLINE basicUnsafeCopy #-}
-  {-# INLINE basicUnsafeGrow #-}
-  basicLength (MV_Maybe v) = M.basicLength v
-  basicUnsafeSlice i n (MV_Maybe v) = MV_Maybe $ M.basicUnsafeSlice i n v
-  basicOverlaps (MV_Maybe v1) (MV_Maybe v2) = M.basicOverlaps v1 v2
-  basicUnsafeNew n = MV_Maybe `liftM` M.basicUnsafeNew n
-  basicInitialize (MV_Maybe v) = M.basicInitialize v
-  basicUnsafeReplicate n m = MV_Maybe `liftM` M.basicUnsafeReplicate n (fromMaybe m)
-  basicUnsafeRead (MV_Maybe v) i = toMaybe `liftM` M.basicUnsafeRead v i
-  basicUnsafeWrite (MV_Maybe v) i m = M.basicUnsafeWrite v i (fromMaybe m)
-  basicClear (MV_Maybe v) = M.basicClear v
-  basicSet (MV_Maybe v) m = M.basicSet v (fromMaybe m)
-  basicUnsafeCopy (MV_Maybe v1) (MV_Maybe v2) = M.basicUnsafeCopy v1 v2
-  basicUnsafeMove (MV_Maybe v1) (MV_Maybe v2) = M.basicUnsafeMove v1 v2
-  basicUnsafeGrow (MV_Maybe v) n = MV_Maybe `liftM` M.basicUnsafeGrow v n
-
-instance (U.Unbox a) => G.Vector U.Vector (Maybe a) where
-  {-# INLINE basicUnsafeFreeze #-}
-  {-# INLINE basicUnsafeThaw #-}
-  {-# INLINE basicLength #-}
-  {-# INLINE basicUnsafeSlice #-}
-  {-# INLINE basicUnsafeIndexM #-}
-  {-# INLINE elemseq #-}
-  basicUnsafeFreeze (MV_Maybe v) = V_Maybe `liftM` G.basicUnsafeFreeze v
-  basicUnsafeThaw (V_Maybe v) = MV_Maybe `liftM` G.basicUnsafeThaw v
-  basicLength (V_Maybe v) = G.basicLength v
-  basicUnsafeSlice i n (V_Maybe v) = V_Maybe $ G.basicUnsafeSlice i n v
-  basicUnsafeIndexM (V_Maybe v) i = toMaybe `liftM` G.basicUnsafeIndexM v i
-  basicUnsafeCopy (MV_Maybe mv) (V_Maybe v) = G.basicUnsafeCopy mv v
-  elemseq _ (Just a) z = G.elemseq (undefined :: U.Vector a) a z
-  elemseq _ Nothing   z = z
+instance (U.Unbox b) => G.Vector U.Vector (Maybe b) where
+  {-# INLINE basicUnsafeFreeze  #-}
+  basicUnsafeFreeze (MV_Maybe n_ as bs)
+      = do
+          as' <- G.basicUnsafeFreeze as
+          bs' <- G.basicUnsafeFreeze bs
+          return $ V_Maybe n_ as' bs'
+  {-# INLINE basicUnsafeThaw  #-}
+  basicUnsafeThaw (V_Maybe n_ as bs)
+      = do
+          as' <- G.basicUnsafeThaw as
+          bs' <- G.basicUnsafeThaw bs
+          return $ MV_Maybe n_ as' bs'
+  {-# INLINE basicLength  #-}
+  basicLength (V_Maybe n_ _ _) = n_
+  {-# INLINE basicUnsafeSlice  #-}
+  basicUnsafeSlice i_ m_ (V_Maybe _ as bs)
+      = V_Maybe m_ (G.basicUnsafeSlice i_ m_ as)
+               (G.basicUnsafeSlice i_ m_ bs)
+  {-# INLINE basicUnsafeIndexM  #-}
+  basicUnsafeIndexM (V_Maybe _ as bs) i_
+      = do
+          a <- G.basicUnsafeIndexM as i_
+          if a
+            then do
+              b <- G.basicUnsafeIndexM bs i_
+              return (Just b)
+            else do
+              return Nothing
+  {-# INLINE basicUnsafeCopy  #-}
+  basicUnsafeCopy (MV_Maybe _ as1 bs1) (V_Maybe _ as2 bs2)
+      = do
+          G.basicUnsafeCopy as1 as2
+          G.basicUnsafeCopy bs1 bs2
+  {-# INLINE elemseq  #-}
+  elemseq _ (Just b ) z
+      = G.elemseq (undefined :: U.Vector b) b z
+  elemseq _ (Nothing) z
+      = z
